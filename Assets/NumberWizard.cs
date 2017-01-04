7906a26ee4dcbedbef76873d585fd4c9dfb6bdaa
JedraPeake <jpeake2@uwo.ca>
2017-01-04 17:57:01 -0500

    Initial commit

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+	
+	int max;
+	int min;
+	int guess;
+	
+	public int maxGuessesAllowed =5;
+	
+	public Text text;
+	
+	// Use this for initialization
+	void Start () {
+		StartGame();
+	}
+	
+	void StartGame(){
+		max = 1001;
+		min =1;
+		NextGuess();
+	}
+	
+	// Update is called once per frame
+	
+	
+	public void GuessHigher()
+	{
+		min =guess;
+		NextGuess();
+	}
+	public void GuessLower()
+	{
+		max =guess;
+		NextGuess();
+	}
+		
+	void NextGuess(){
+		guess = Random.Range(min,max+1);
+		text.text =guess.ToString();
+		maxGuessesAllowed--;
+		if(maxGuessesAllowed==0)
+		{
+			Application.LoadLevel("Win");
+		}
+	}
+}
